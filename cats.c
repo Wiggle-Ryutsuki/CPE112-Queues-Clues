6c00c9125d90056b38fab395acffd45683278122
Maimoona <maimoonaaz15@gmail.com>
2025-04-26 13:41:41 +0700

    cat functions

---
+++ cats.c
@@ -0,0 +1,51 @@
+#include "cats.h"
+
+void cutecat(){
+    printf("  /\\_/\\   \n");
+    printf(" ( ^w^ )  \n");
+    printf("  > ^ <   \n");
+    printf(" /     \\ \n");
+    printf(" \\__|__/  \n");
+}
+
+
+void grumpycat(){
+    printf(" /\\_/\\\n");
+    printf("( =.= )\n");
+    printf("  ----\n");
+    printf("/     \\ \n");
+    printf("\\__|__/  \n");
+}
+
+
+void dumbcat(){
+    printf(" /\\_/\\\n");
+    printf("( o o )\n");
+    printf("==_Y_==\n");
+    printf("/     \\ \n");
+    printf("\\__|__/  \n");
+}
+
+
+// Random cat generator
+void randomCat(){
+    srand(time(NULL));
+
+    // Generate number from 1 to 3
+    int number = rand() % 3 + 1;
+
+    // Random cats
+    switch (number) {
+        case 1:
+            cutecat();
+            break;
+        case 2:
+            grumpycat();
+            break;
+        case 3:
+            dumbcat();
+            break;
+        default:
+            printf("An error has occured when generating random cat.\n");
+    }
+}
---
+++ cats.h
@@ -0,0 +1,13 @@
+#ifndef CATS_H
+#define CATS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void cutecat();
+void grumpycat();
+void dumbcat();
+void randomCat();
+
+#endif
